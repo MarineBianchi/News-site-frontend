0b1793011001102a7b160b0e67a3972b5878cb53
Marine Bianchi <marine.bianchi@gmail.com>
2023-07-23 22:53:20 +0200

    final frontend

---
+++ components/Home.js
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import Head from 'next/head';
+import Article from './Article';
+import TopArticle from './TopArticle';
+import styles from '../styles/Home.module.css';
+
+function Home() {
+  const bookmarks = useSelector((state) => state.bookmarks.value);
+  const hiddenArticles = useSelector((state) => state.hiddenArticles.value);
+
+  const [articlesData, setArticlesData] = useState([]);
+  const [topArticle, setTopArticle] = useState({});
+
+  // fetch de la route pour display les articles
+
+  useEffect(() => {
+    fetch('http://localhost:3000/articles')
+      .then(response => response.json())
+      .then(data => {
+        setTopArticle(data.articles[0]);
+        setArticlesData(data.articles.filter((data, i) => i > 0));
+      });
+  }, []);
+
+  // fonction filter (comme find) = renvoie un nouveau tableau, passe test true/false =  si hiddenArticles n'est pas trouvé dans articlesdata on l'affiche, si non ne passe pas le test n'est pas affiché
+
+  const filteredArticles = articlesData.filter((data) => !hiddenArticles.includes(data.title));
+
+
+// on utilise le reducer pour créer une props isBookmarked et l'exporter dans les composants
+
+  const articles = filteredArticles.map((data, i) => {
+    const isBookmarked = bookmarks.some(bookmark => bookmark.title === data.title);
+    return <Article key={i} {...data} isBookmarked={isBookmarked} />;
+  });
+
+
+  if (bookmarks.some(bookmark => bookmark.title === topArticle.title)) {
+    topArticles = <TopArticle {...topArticle} isBookmarked={true} />
+  } else {
+    topArticles = <TopArticle {...topArticle} isBookmarked={false} />
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>Morning News - Home</title>
+      </Head>
+      {topArticles}
+      <div className={styles.articlesContainer}>
+        {articles}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
